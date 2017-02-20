bc64d57dfa60c200e09aafe0ca8ac8f552372896
Clint <clintvencor@gmail.com>
2017-02-20 12:29:27 +0000

    init db

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
     belongs_to :supplier, dependent: :destroy
+    validates :price, presence: true
+    validates :product_name, presence: true
+    validates :description, presence: true
 end
